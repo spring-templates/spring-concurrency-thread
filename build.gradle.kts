d72767fe7c602b141488525c7f5d95f550e2b8bb
Hyeon-hak Kim <07ily@naver.com>
2024-03-24 22:02:15 +0900

    🚚 Chore : 프로젝트 초기화 (#9)
    * ✨ Feat : `.gitmessage`
    
    - import from `spring-templates/java-lotto`
    - 백엔드 개발 간 자주 사용될 것으로 예상되는 내용으로 수정
    
    * 🚚 Chore : Spring 프로젝트 초기화
    
    - Java/Spring Boot
    - BellSoft Liberica JDK 17
    - Spring Web
    - [spring initializr](https://start.spring.io/#!type=gradle-project-kotlin&language=java&platformVersion=3.2.4&packaging=jar&jvmVersion=17&groupId=com.concurrency&artifactId=thread&name=spring-concurrency-thread&description=Examples%20of%20Java%20Thread%20Concurrency%20in%20Spring%20Framework&packageName=com.concurrency.thread&dependencies=web)
    
    * 🐛 Fix : 경고 억제를 위한 빌드 설정
    
    - gradle deprecated usage
    - jvm CDS
    
    * ✨ Feat : Build Optimizing
    
    - enable build caching
    - enable CDS while execution
    
    * 🚚 Chore : GitHub Actions 통합 테스트
    
    * 🐛 Fix : 유효하지 않은 캐시키 이슈
    
    - `.gradle/caches`에 의존성 나열
    
    * 🐛 Fix : remove redundancy
    
    - GitHub Actions의 자체 캐싱 활용
    
    * ✨ Feat : test 정책 변경
    
    - `main` 브랜치만 clean test 수행
    
    * 🐛 Fix : 캐싱 기능
    
    * 🐛 Fix : `main` 이외 브랜치 캐싱
    
    * 📝 Docs : PR template
    
    * 🐛 Fix : `.gitmessage` format 추가
    
    * 🎨 Style : polishing
    
    * 🚚 Chore : simplify dependency

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.thread"
+version = "0.1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+}
+
+tasks {
+    all {
+        outputs.cacheIf { true }
+    }
+}
+
+tasks.test {
+    jvmArgs("-Xshare:off")
+    useJUnitPlatform()
+}
+
+apply(from = "dumpJsa.gradle.kts")
+tasks.withType<JavaExec> {
+    dependsOn("dumpJsa")
+}
---
+++ src/main/java/com/thread/concurrency/SpringThreadConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.thread.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThreadConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThreadConcurrencyApplication.class, args);
+    }
+
+}

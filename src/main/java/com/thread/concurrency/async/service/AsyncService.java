fd930569f6ff7449ee39d63cd716334146344a7c
ohchansol <haxr369@gmail.com>
2024-03-27 09:46:14 +0900

    ✅ Test : 테스트 코드 추가
    1. 메서드가 비동기 메서드인지 확인하는 테스트 작성
    2. @Async를 붙인 메서드가 비동기로 동자가하지 않고 하나의 쓰레드에서 동작한다.

--- src/main/java/com/thread/concurrency/async/service/AsyncService.java
+++ src/main/java/com/thread/concurrency/async/service/AsyncService.java
@@ -8,11 +8,11 @@ import java.util.concurrent.CompletableFuture;
 @Service
 public class AsyncService {
     @Async
-    public CompletableFuture<String> voidParamStringReturn(){
-        System.out.println("비동기적으로 실행 - "+
-            Thread.currentThread().getName());
+    public CompletableFuture<String> voidParamStringReturn(long waitTime){
+//        System.out.println("비동기적으로 실행 - "+
+//            Thread.currentThread().getName());
         try{
-            Thread.sleep(1000);
+            Thread.sleep(waitTime);
             return CompletableFuture.completedFuture("hello world");
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
--- src/test/java/com/thread/concurrency/AsyncServiceTest.java
+++ src/test/java/com/thread/concurrency/AsyncServiceTest.java
@@ -1,37 +1,61 @@
 package com.thread.concurrency;
 
 import com.thread.concurrency.async.service.AsyncService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
-
+@SpringBootTest
 public class AsyncServiceTest {
+    @Autowired
     private AsyncService asyncService;
-    @BeforeEach
-    void initUseCase() { // spring container를 사용하지 않고 순수 클래스를 사용.
-        asyncService = new AsyncService();
-    }
     @Test
     @DisplayName("입력은 void 출력은 String인 비동기 함수 단일 호출")
     public void testGetString() throws ExecutionException, InterruptedException {
-        CompletableFuture<String> helloWorld = asyncService.voidParamStringReturn();
+        CompletableFuture<String> helloWorld = asyncService.voidParamStringReturn(1000);
         Assertions.assertEquals("hello world",helloWorld.get());
     }
+    @Test
+    @DisplayName("입력은 void 출력은 String인 비동기 함수 단일 호출 타임아웃 발생.")
+    public void testGetStringTimeOutIsThisAsync() {
+        // voidParamStringReturn가 비동기 메서드인지 의문이 생김.
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                return asyncService.voidParamStringReturn(4000).get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        long timeOutValue = 1;
+        TimeUnit timeUnit = TimeUnit.SECONDS;
+        // 1초가 지난 후 타임아웃 발생
+        Assertions.assertThrows(ExecutionException.class, () -> completableFuture.orTimeout(timeOutValue,timeUnit).get());
+    }
 
-//    @Test
-//    @DisplayName("입력은 void 출력은 String인 비동기 함수 복수 호출 그리고 결과 조합")
-//    public void testMultiGetString() throws ExecutionException, InterruptedException {
-//        List<CompletableFuture<String>> completableFutures = new ArrayList<>();
-//
-//        for (int j = 0; j <= 23; j++) {
-//            completableFutures.add(asyncService.voidParamStringReturn()));
-//        }
-//        CompletableFuture.allOf(price1,price2,price3).join();
-//        Assertions.assertEquals("hello world !!hello world !!", combin);
-//    }
+    @Test
+    @DisplayName("입력은 void 출력은 String인 비동기 함수 복수 호출 그리고 결과 조합")
+    public void testMultiGetString() {
+        List<CompletableFuture<String>> futures = new ArrayList<>();
+        for (int i = 1; i <= 1000; i++) { // 동기라면 10초가 걸리고 비동기라면 0.01초가 걸릴 것이다.
+            futures.add(asyncService.voidParamStringReturn(10));
+        }
+        CompletableFuture<List<String>> aggregate = CompletableFuture.completedFuture(new ArrayList<>());
+        for (CompletableFuture<String> future : futures) {
+            aggregate = aggregate.thenCompose(list -> {
+                list.add(String.valueOf(future));
+                return CompletableFuture.completedFuture(list);
+            });
+        }
+        final List<String> results = aggregate.join();
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("Finished " + results.get(i));
+        }
+
+    }
 }

423c09af131b2a0e7aa73464eb64a9f0da145d3b
Hyeon-hak Kim <07ily@naver.com>
2024-03-28 02:00:57 +0900

    🚚 Chore : 프로젝트 초기화 (#18)
    * ✨ Feat : `.gitmessage`
    
    - import from `spring-templates/java-lotto`
    - 백엔드 개발 간 자주 사용될 것으로 예상되는 내용으로 수정
    
    * 🚚 Chore : Spring 프로젝트 초기화
    
    - Java/Spring Boot
    - BellSoft Liberica JDK 17
    - Spring Web
    - [spring initializr](https://start.spring.io/#!type=gradle-project-kotlin&language=java&platformVersion=3.2.4&packaging=jar&jvmVersion=17&groupId=com.concurrency&artifactId=thread&name=spring-concurrency-thread&description=Examples%20of%20Java%20Thread%20Concurrency%20in%20Spring%20Framework&packageName=com.concurrency.thread&dependencies=web)
    
    * 🐛 Fix : 경고 억제를 위한 빌드 설정
    
    - gradle deprecated usage
    - jvm CDS
    
    * ✨ Feat : Build Optimizing
    
    - enable build caching
    - enable CDS while execution
    
    * 🚚 Chore : GitHub Actions 통합 테스트
    
    * 🐛 Fix : 유효하지 않은 캐시키 이슈
    
    - `.gradle/caches`에 의존성 나열
    
    * 🐛 Fix : remove redundancy
    
    - GitHub Actions의 자체 캐싱 활용
    
    * ✨ Feat : test 정책 변경
    
    - `main` 브랜치만 clean test 수행
    
    * 🐛 Fix : 캐싱 기능
    
    * 🐛 Fix : `main` 이외 브랜치 캐싱
    
    * 📝 Docs : PR template
    
    * 🐛 Fix : `.gitmessage` format 추가
    
    * 🎨 Style : polishing
    
    * 🚚 Chore : simplify dependency
    
    * ✨ Feat : JDK 21 with Jacoco (#15)
    
    * 🚚 Chore : 프로젝트 초기화 (#9)
    
    * ✨ Feat : `.gitmessage`
    
    - import from `spring-templates/java-lotto`
    - 백엔드 개발 간 자주 사용될 것으로 예상되는 내용으로 수정
    
    * 🚚 Chore : Spring 프로젝트 초기화
    
    - Java/Spring Boot
    - BellSoft Liberica JDK 17
    - Spring Web
    - [spring initializr](https://start.spring.io/#!type=gradle-project-kotlin&language=java&platformVersion=3.2.4&packaging=jar&jvmVersion=17&groupId=com.concurrency&artifactId=thread&name=spring-concurrency-thread&description=Examples%20of%20Java%20Thread%20Concurrency%20in%20Spring%20Framework&packageName=com.concurrency.thread&dependencies=web)
    
    * 🐛 Fix : 경고 억제를 위한 빌드 설정
    
    - gradle deprecated usage
    - jvm CDS
    
    * ✨ Feat : Build Optimizing
    
    - enable build caching
    - enable CDS while execution
    
    * 🚚 Chore : GitHub Actions 통합 테스트
    
    * 🐛 Fix : 유효하지 않은 캐시키 이슈
    
    - `.gradle/caches`에 의존성 나열
    
    * 🐛 Fix : remove redundancy
    
    - GitHub Actions의 자체 캐싱 활용
    
    * ✨ Feat : test 정책 변경
    
    - `main` 브랜치만 clean test 수행
    
    * 🐛 Fix : 캐싱 기능
    
    * 🐛 Fix : `main` 이외 브랜치 캐싱
    
    * 📝 Docs : PR template
    
    * 🐛 Fix : `.gitmessage` format 추가
    
    * 🎨 Style : polishing
    
    * 🚚 Chore : simplify dependency
    
    * ✨ Feat : JDK 21 with Jacoco
    
    * 🔧 Modify : codecov-action 테스트
    
    [codecov-action](https://github.com/codecov/codecov-action)

--- src/test/java/com/thread/concurrency/SpringThreadConcurrencyApplicationTests.java
+++ src/test/java/com/thread/concurrency/SpringThreadConcurrencyApplicationTests.java
@@ -2,12 +2,14 @@ package com.thread.concurrency;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class SpringThreadConcurrencyApplicationTests {
 
     @Test
     void contextLoads() {
+        assertDoesNotThrow(() -> SpringThreadConcurrencyApplication.main(new String[]{}));
     }
 
 }

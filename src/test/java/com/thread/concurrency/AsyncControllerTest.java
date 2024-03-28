c734c167888058b7b6275052cce642b79896531b
ohchansol <haxr369@gmail.com>
2024-03-28 12:32:08 +0900

    ✨ Feat : 새로운 기능 ✅ Test : 테스트 코드 추가
    1. 비동기 호출을 10회 하는 비동기 메서드를 10회 호출하는 calculateRunTime 작성
    2. 비동기 호출이 100회 일 때 50개는 쓰레드가 실행하고 나머지 50개는 블록킹 큐에서 대기한다.

---
+++ src/test/java/com/thread/concurrency/AsyncControllerTest.java
@@ -0,0 +1,32 @@
+package com.thread.concurrency;
+
+import com.thread.concurrency.async.controller.AsyncController;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@SpringBootTest
+public class AsyncControllerTest {
+    private static final Logger logger = LoggerFactory.getLogger(AsyncServiceTest.class);
+
+    @Autowired
+    private AsyncController asyncController;
+
+    @Test
+    public void invokeMultiAsyncMethod() throws InterruptedException {
+        List<CompletableFuture<String>> hellos = new ArrayList<>();
+        for(int i=0; i<10; i++){
+            hellos.add(asyncController.calculateRunTime(10, 1000));
+        }
+        // 모든 비동기 호출이 완료될 때까지 대기하고 결과를 리스트에 넣기
+        List<String> results = hellos.stream().map(CompletableFuture::join)
+            .toList();
+        results.forEach(logger::info);
+    }
+}

b77d4834f98c7c91909e23c62cb43bc1a1c44da4
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-04-01 09:59:55 +0900

    ✅ Test : `stressTest` 작성
    Signed-off-by: Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>

--- src/test/java/com/thread/concurrency/counter/CounterTest.java
+++ src/test/java/com/thread/concurrency/counter/CounterTest.java
@@ -1,70 +1,64 @@
 package com.thread.concurrency.counter;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.CountDownLatch;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static java.lang.Thread.sleep;
 
 @SpringBootTest
 public class CounterTest {
 
     public static Stream<Counter> counterProvider() {
-        return Stream.of(new BatchingCounter(), new LockCounter(), new PollingCounter());
+        return Stream.of(new BatchingCounter(), new LockCounter(), new PollingCounter(), new BasicCounter());
+    }
+
+    private static void assertThen(Counter counter, int expectedValue, int actualValue) {
+        System.out.println("Expected value: " + expectedValue);
+        System.out.println("Actual value: " + actualValue);
+        if (counter instanceof BasicCounter) {
+            System.out.println("BasicCounter is not thread-safe");
+            Assertions.assertNotEquals(expectedValue, actualValue);
+        } else {
+            System.out.println("Counter is thread-safe");
+            Assertions.assertEquals(expectedValue, actualValue);
+        }
     }
 
     @ParameterizedTest
     @MethodSource("counterProvider")
-    public void testAddInMultithreadedEnvironment(
-        Counter counter
-    ) throws InterruptedException {
+    public void stressTest(Counter counter) throws InterruptedException {
         int initialValue = counter.show();
         int nThreads = 100;
         int nAddsPerThread = 1000;
         int valueToAdd = 1;
+        int expectedValue = initialValue + nThreads * nAddsPerThread * valueToAdd;
 
-        try (ExecutorService executorService = Executors.newFixedThreadPool(nThreads)) {
-            for (int i = 0; i < nThreads; i++) {
-                executorService.submit(() -> {
-                    for (int j = 0; j < nAddsPerThread; j++) {
-                        counter.add(valueToAdd);
-                    }
-                });
+
+        // define runnable job
+        CountDownLatch latch = new CountDownLatch(nThreads);
+        Runnable job = () -> {
+            try {
+                latch.countDown(); // decrease the count
+                latch.await(); // wait until the count reaches 0
+                for (int i = 0; i < nAddsPerThread; i++) {
+                    counter.add(valueToAdd);
+                }
+            } catch (InterruptedException ignored) {
             }
-            if (counter instanceof BatchingCounter) {
-                Thread.sleep(500);
-            }
+        };
+
+        // start nThreads threads
+        for (int i = 0; i < nThreads; i++) {
+            Thread.ofVirtual().start(job);
         }
 
-        int expectedValue = initialValue + nThreads * nAddsPerThread * valueToAdd;
-        assertEquals(expectedValue, counter.show());
-    }
+        sleep(300); // wait for all threads to finish
 
-    @Test
-    public void testMalfunction() {
-        var counter = new BasicCounter();
-        int initialValue = counter.show();
-        int nThreads = 100;
-        int nAddsPerThread = 1000;
-        int valueToAdd = 1;
-
-        try (ExecutorService executorService = Executors.newFixedThreadPool(nThreads)) {
-            for (int i = 0; i < nThreads; i++) {
-                executorService.submit(() -> {
-                    for (int j = 0; j < nAddsPerThread; j++) {
-                        counter.add(valueToAdd);
-                    }
-                });
-            }
-        }
-
-        int expectedValue = initialValue + nThreads * nAddsPerThread * valueToAdd;
-        assertNotEquals(expectedValue, counter.show());
+        assertThen(counter, expectedValue, counter.show());
     }
 }

bad08f5762db5e0d91a28b80de7c064423d3c552
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-04-09 10:50:55 +0900

    ✨ Feat (Counter) : custom test executor
    Signed-off-by: Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>

--- src/main/java/com/thread/concurrency/SpringThreadConcurrencyApplication.java
+++ src/main/java/com/thread/concurrency/SpringThreadConcurrencyApplication.java
@@ -1,96 +1,17 @@
 package com.thread.concurrency;
 
-import com.thread.concurrency.counter.batch.BatchCounter;
-import com.thread.concurrency.counter.batch.ConcurrentBatchingCounter;
+import com.thread.concurrency.executor.CounterBenchmark;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryMXBean;
-import java.lang.management.MemoryUsage;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.function.Consumer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-@EnableAsync
 public class SpringThreadConcurrencyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringThreadConcurrencyApplication.class, args);
-
-        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
-        MemoryUsage initialMemoryUsage = memoryMXBean.getHeapMemoryUsage();
-        long initialTime = System.currentTimeMillis();
-
-        // Run the test
-        int totalRequest = Integer.MAX_VALUE / 1024;
-        conditionalMultiThreading(totalRequest);
-
-        MemoryUsage finalMemoryUsage = memoryMXBean.getHeapMemoryUsage();
-        long finalTime = System.currentTimeMillis();
-        long elapsedTime = finalTime - initialTime;
-        long usedMemory = finalMemoryUsage.getUsed() - initialMemoryUsage.getUsed();
-
-        // request with comma
-        System.out.println("Total request: " + String.format("%,d", totalRequest));
-        // seconds
-        System.out.println("Elapsed time: " + elapsedTime / 1000 + " s");
-        // megabytes
-        System.out.println("Used memory: " + usedMemory / 1024 / 1024 + " MB");
-    }
-
-    private static void conditionalMultiThreading(int expected) {
-        BatchCounter counter = new ConcurrentBatchingCounter();
-
-        // given
-        int numberOfThreads = 128;
-        List<Integer> iterPerThread = range(numberOfThreads, expected);
-        Consumer<Integer> task = (Integer number) -> {
-            for (int i = 0; i < number; i++) {
-                counter.add(1);
-            }
-            counter.flush();
-        };
-        // when
-        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            List<CompletableFuture<Void>> futures = iterPerThread.stream().map(number -> CompletableFuture.runAsync(() -> task.accept(number), executor)).toList();
-            futures.forEach(CompletableFuture::join);
-        }
-        // then
-        assert expected == counter.show();
-    }
-
-    private static List<Integer> range(int numberOfThreads, int expected) {
-        int baseValue = expected / numberOfThreads;
-        int remainder = expected % numberOfThreads;
-
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < numberOfThreads; i++) {
-            if (i < remainder) {
-                result.add(baseValue + 1);
-            } else {
-                result.add(baseValue);
-            }
-        }
-        return result;
-    }
-
-    @Bean
-    public Executor taskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor(); // Spring에서 사용하는 스레드를 제어한느 설정
-        executor.setCorePoolSize(50); // thread-pool에 살아있는 thread의 최소 개수
-        executor.setMaxPoolSize(50);  // thread-pool에서 사용할 수 있는 최대 개수
-        executor.setQueueCapacity(500); //thread-pool에 최대 queue 크기
-        executor.setThreadNamePrefix("AsyncApp-");
-        executor.initialize();
-        return executor;
+        ConfigurableApplicationContext context = SpringApplication.run(SpringThreadConcurrencyApplication.class, args);
+        var performance = context.getBean(CounterBenchmark.class).benchmark();
+        System.out.println("|----------------------|---------------|---------------|---------------|");
+        System.out.println(performance);
     }
 }
---
+++ src/main/java/com/thread/concurrency/executor/CounterBenchmark.java
@@ -0,0 +1,105 @@
+package com.thread.concurrency.executor;
+
+import com.thread.concurrency.counter.Counter;
+import com.thread.concurrency.counter.batch.BatchCounter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
+
+@Component
+public class CounterBenchmark {
+    private final Counter counter;
+    private final String counterName;
+    private final int iterations, nThreads, totalRequests;
+
+    @Autowired
+    public CounterBenchmark(CounterConfiguration.CounterConfig config) {
+        System.out.println(config);
+        this.counter = config.counter();
+        this.counterName = counter.getClass().getSimpleName();
+        this.iterations = config.iterations();
+        this.nThreads = config.nThreads();
+        this.totalRequests = config.totalRequests();
+    }
+
+    private void doAdd(List<Integer> params, Consumer<Integer> task) {
+        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            counter.clear();
+            List<CompletableFuture<Void>> futures = new ArrayList<>();
+            for (int i = 0; i < nThreads; i++) {
+                int nRequest = params.get(i);
+                futures.add(CompletableFuture.runAsync(() -> task.accept(nRequest), executor));
+            }
+            futures.forEach(CompletableFuture::join);
+        }
+    }
+
+    public Performance benchmark() {
+        System.out.printf("| %-20s | %13s | %13s | %13s |%n", "Name", "Time", "Threads", "Memory");
+        System.out.println("|----------------------|---------------|---------------|---------------|");
+        List<Performance> performances = new ArrayList<>();
+        for (int i = 0; i < iterations; i++) {
+            var performance = calculateEach();
+            performances.add(performance);
+            System.out.println(performance);
+        }
+        return reduce(performances);
+    }
+
+    private Performance calculateEach() {
+        List<Integer> iterPerThread = range();
+        Consumer<Integer> task = (Integer nRequest) -> {
+            for (int i = 0; i < nRequest; i++) {
+                counter.add(1);
+            }
+            if (counter instanceof BatchCounter batchCounter) {
+                batchCounter.flush();
+            }
+        };
+        System.gc();
+        long timeOnStart = System.currentTimeMillis();
+        long memoryOnStart = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        doAdd(iterPerThread, task);
+        if (counter.show() != totalRequests) {
+            System.out.printf("Counter: %d, Total: %d%n", counter.show(), totalRequests);
+        }
+
+
+        long timeOnEnd = System.currentTimeMillis();
+        long memoryOnEnd = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+
+        long timeElapsed = timeOnEnd - timeOnStart;
+        long memoryUsed = memoryOnEnd - memoryOnStart;
+        return new Performance(counterName, timeElapsed, iterPerThread.size(), memoryUsed);
+    }
+
+    private List<Integer> range() {
+        int baseValue = totalRequests / nThreads;
+        int remainder = totalRequests % nThreads;
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nThreads; i++) {
+            var remainderValue = i < remainder ? 1 : 0;
+            result.add(baseValue + remainderValue);
+        }
+        assert result.stream().mapToInt(Integer::intValue).sum() == totalRequests;
+        return result;
+    }
+
+    private Performance reduce(List<Performance> performances) {
+        performances.sort((a, b) -> (int) (a.time() - b.time()));
+        long time = performances.get(performances.size() / 2).time();
+        performances.sort((a, b) -> (int) (a.memory() - b.memory()));
+        long memory = performances.get(performances.size() / 2).memory();
+        return new Performance(counterName, time, nThreads, memory);
+    }
+}
